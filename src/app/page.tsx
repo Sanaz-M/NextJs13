7da0b9600f39ddaf6167e95204072173217da67d
Sanaz M <sanazmohammaddi@gmail.com>
2023-07-18 16:28:19 +0100

    updating the Paragraph component
    assign proper types, using dynamic class name, and using forwardRef from react library.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { Inter } from 'next/font/google';
+
+import Paragraph from './components/ui/Paragraph';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className=''>Hello</h2>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-400">
+      <Paragraph />
     </main>
   )
 }
