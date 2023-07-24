f8ab11b9589463285e960925508fa4b7047f3974
Sanaz M <sanazmohammaddi@gmail.com>
2023-07-24 17:25:51 +0100

    create Toast and toaster functions
    Create these functions to catch the error and display it to user.
    
    Also update the tailwind config for new themes and dark mode

--- src/app/components/SignOutButton.tsx
+++ src/app/components/SignOutButton.tsx
@@ -4,6 +4,7 @@ import { FC, useState } from 'react';
 import { signOut } from 'next-auth/react';
 
 import Button from '@/ui/Button';
+import { toast } from '@/ui/Toast';
 
 interface SignOutButtonProps {
 
@@ -18,11 +19,11 @@ const SignOutButton: FC<SignOutButtonProps> = ({ }) => {
     try {
       await signOut();
     } catch (error) {
-      // toast({
-      //   title: 'Error signing out',
-      //   message: "Please try again later!",
-      //   type: 'error',
-      // })
+      toast({
+        title: 'Error signing out',
+        message: "Please try again later!",
+        type: 'error',
+      })
     }
   }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import Providers from '@/components/Providers';
 import NavBar from '@/components/NavBar';
+import { Toaster } from '@/components/ui/Toast';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en" className={cn("bg-white text-slate-900 antialiased", inter.className)}>
       <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
         <Providers>{children}
+          <Toaster position='bottom-right' />
           <NavBar />
         </Providers>
 
