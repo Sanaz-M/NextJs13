94655b3bc8c8889e171552e89bd44de87105095f
Sanaz M <sanazmohammaddi@gmail.com>
2023-07-25 16:34:34 +0100

    create 2 new components, Code and Tabs

--- src/app/components/DocumentationTabs.tsx
+++ src/app/components/DocumentationTabs.tsx
@@ -1,11 +1,31 @@
+'use client'
+
 import { FC } from 'react';
+//SimpleBar is meant to improve the experience of internal web page scrolling; such as a chat box or a small scrolling area.
+import SimpleBar from 'simplebar-react';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/ui/Tabs';
+import Code from './ui/Code';
 
 const DocumentationTabs: FC = () => {
   return (
-    <div>
-
-    </div>
-  );
+    <Tabs defaultValue='nodejs' className='max-w-2xl w-full'>
+      <TabsList>
+        <TabsTrigger value='nodejs'>NodeJS</TabsTrigger>
+        <TabsTrigger value='python'>Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value='nodejs'>
+        <SimpleBar forceVisible='y'>
+          <Code animated code="nodejs" language='javascript' show />
+        </SimpleBar>
+      </TabsContent>
+      <TabsContent value='python'>
+        <SimpleBar forceVisible='y'>
+          <Code animated code="python" language='python' show />
+        </SimpleBar>
+      </TabsContent>
+    </Tabs>
+  )
 }
 
-export default DocumentationTabs;
+export default DocumentationTabs
