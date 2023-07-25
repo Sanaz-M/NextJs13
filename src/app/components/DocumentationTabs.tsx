c9af86c609d2618f1b8f7ba72c9dff78d779f79f
Sanaz M <sanazmohammaddi@gmail.com>
2023-07-25 15:38:42 +0100

    create a new page and layout under (docs) folder
    The folders in parenthesis will be omitted from the URL. Even though routes inside (docs) share the same URL hierarchy, you can create a different layout for each group by adding a layout.js file inside their folders.

---
+++ src/app/components/DocumentationTabs.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const DocumentationTabs: FC = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default DocumentationTabs;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,54 @@
-import Paragraph from './components/ui/Paragraph';
+import LargeHeading from "@/ui/LargeHeading";
+import Paragraph from "@/ui/Paragraph";
+
+import type { Metadata } from "next";
+import Link from 'next/link';
+import Image from 'next/image';
+
+
+//Next.js has a Metadata API that can be used to define your application metadata 
+//(e.g. meta and link tags inside your HTML head element) for improved SEO and web shareability.
+export const metadata: Metadata = {
+  title: "Similarity API",
+  description: "Clone- Free & open-source text similarity API",
+};
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Paragraph size="sm">Some Text</Paragraph>
+    <main className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className='container pt-32 max-w-7xl w-full mx-auto h-full'>
+        <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start'>
+          <LargeHeading
+            size='lg'
+            className='three-d text-black dark:text-light-gold'>
+            Easily determine <br /> text similarity.
+          </LargeHeading>
+
+          <Paragraph className='max-w-xl lg:text-left'>
+            With the Text Similarity API, you can easily determine the
+            similarity between two pieces of text with a free{' '}
+            <Link
+              href='/login'
+              className='underline underline-offset-2 text-black dark:text-light-gold'>
+              API key
+            </Link>
+            .
+          </Paragraph>
+
+          <div className='relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute'>
+            <Image
+              priority
+              className='img-shadow '
+              quality={100}
+              style={{ objectFit: 'contain' }}
+              fill
+              src='/typewriter.png'
+              alt='typewriter'
+            />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
