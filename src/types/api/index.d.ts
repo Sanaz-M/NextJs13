b8a37cb972d0eedf7426122e27e53691c4f7036c
Sanaz M <sanazmohammaddi@gmail.com>
2023-08-11 17:32:17 +0100

    new files to handle the APi reguests

--- src/types/api/index.d.ts
+++ src/types/api/index.d.ts
@@ -1,7 +1,8 @@
 import { ApiKey } from '@prisma/client';
 import { ZodIssue } from 'zod';
 
-//Zod is a TypeScript-first schema declaration and validation library.
+// Zod is a TypeScript-first schema declaration and validation library.
+// Zod is designed to be as developer-friendly as possible. The goal is to eliminate duplicative type declarations.
 
 export interface CreateApiData {
   error: string | ZodIssue[] | null
